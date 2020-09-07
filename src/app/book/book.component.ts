7f14efff41cd5ad42844094414cfd6adc5a73340
gcauchis <gabriel.cauchis@gmail.com>
2020-09-07 20:49:43 +0200

    suite pdf

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+import { Component, OnInit, Input, ViewChild, ElementRef } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
 
-import { Book } from '../book';
-import { BookService } from '../book.service';
+import { Book } from "../book";
+import { BookService } from "../book.service";
 
-import { MessagesService } from '../messages.service';
+import { MessagesService } from "../messages.service";
 
-import { jsPDF } from "jspdf";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+import htmlToPdfmake from "html-to-pdfmake";
 
 @Component({
-  selector: 'app-book',
-  templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css'],
+  selector: "app-book",
+  templateUrl: "./book.component.html",
+  styleUrls: ["./book.component.css"]
 })
 export class BookComponent implements OnInit {
-
-  @ViewChild('htmlBook') htmlBook:ElementRef;
+  @ViewChild("htmlBook") htmlBook: ElementRef;
 
   book: Book;
 
@@ -24,20 +27,19 @@ export class BookComponent implements OnInit {
     private route: ActivatedRoute,
     private bookService: BookService,
     private location: Location,
-    private messagesService: MessagesService) { }
+    private messagesService: MessagesService
+  ) {}
 
   ngOnInit() {
     this.getBook();
   }
 
   getBook(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
+    const id = +this.route.snapshot.paramMap.get("id");
     if (id == 0) {
       this.book = new Book();
-    }
-    else {
-      this.bookService.getBook(id)
-        .subscribe(book => this.book = book);
+    } else {
+      this.bookService.getBook(id).subscribe(book => (this.book = book));
     }
   }
 
@@ -46,44 +48,50 @@ export class BookComponent implements OnInit {
   }
 
   save(): void {
-    this.bookService.updateBook(this.book)
-      .subscribe(() => this.goBack());
+    this.bookService.updateBook(this.book).subscribe(() => this.goBack());
   }
-  
+
   add(): void {
-    this.bookService.addBook(this.book)
-      .subscribe((newBook: Book) => this.book = newBook);
+    this.bookService
+      .addBook(this.book)
+      .subscribe((newBook: Book) => (this.book = newBook));
   }
 
-  public openPDF():void {
-    this.log('openPDF')
-    let DATA = this.htmlBook.nativeElement;
-    let doc = new jsPDF();
-  
-    doc.html(DATA.innerHTML, {
-      callback: function (doc) {
-        doc.save();
+  public openPDF(): void {
+
+    console.log(document);
+    console.log(document.styleSheets);
+    console.log(JSON.stringify(document.styleSheets));
+    /*var css = [];
+    for (var sheeti = 0; sheeti < document.styleSheets.length; sheeti++) {
+      var sheet = document.styleSheets[sheeti];
+      console.log(sheet);
+      var rules = "cssRules" in sheet ? sheet.cssRules : sheet.rules;
+      for (var rulei = 0; rulei < rules.length; rulei++) {
+        var rule = rules[rulei];
+        if ("cssText" in rule) css.push(rule.cssText);
+        else
+          css.push(rule.selectorText + " {\n" + rule.style.cssText + "\n}\n");
       }
-    });
-    doc.output('dataurlnewwindow');
+    }
+    this.log(css);*/
+    //var styles : CSSStyleDeclaration = getComputedStyle(this.htmlBook.nativeElement);
+    var html = htmlToPdfmake(this.htmlBook.nativeElement.innerHTML);
+    pdfMake
+      .createPdf({
+        content: html,
+        styles: document.styleSheets
+      })
+      .open();
   }
 
-  public downloadPDF():void {
-    this.log('downloadPDF')
-    let DATA = this.htmlBook.nativeElement;
-    let doc = new jsPDF();
-
-    let handleElement = {
-      '#editor':function(element,renderer){
-        return true;
-      }
-    };
-    doc.fromHTML(DATA.innerHTML,15,15,{
-      'width': 200,
-      'elementHandlers': handleElement
-    });
-
-    doc.save('angular-demo.pdf');
+  public downloadPDF(): void {
+    var html = htmlToPdfmake(this.htmlBook.nativeElement.innerHTML);
+    pdfMake
+      .createPdf({
+        content: html
+      })
+      .download();
   }
 
   private log(message: string) {
--- src/app/test-pdf/test-pdf.component.ts
+++ src/app/test-pdf/test-pdf.component.ts
@@ -1,76 +1,142 @@
-import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from "@angular/core";
 
-import { MessagesService } from '../messages.service';
+import { MessagesService } from "../messages.service";
 
 import { jsPDF } from "jspdf";
-import html2canvas from 'html2canvas';
+import html2canvas from "html2canvas";
+
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+import htmlToPdfmake from "html-to-pdfmake";
 
 @Component({
-  selector: 'app-test-pdf',
-  templateUrl: './test-pdf.component.html',
-  styleUrls: ['./test-pdf.component.css']
+  selector: "app-test-pdf",
+  templateUrl: "./test-pdf.component.html",
+  styleUrls: ["./test-pdf.component.css"]
 })
 export class TestPDFComponent implements OnInit {
+  @ViewChild("pdfContent") pdfContent: ElementRef;
 
-  @ViewChild('pdfContent') pdfContent:ElementRef;
+  constructor(private messagesService: MessagesService) {}
 
-  constructor(
-    private messagesService: MessagesService) { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-  
-  public openPDF():void {
-    this.log('openPDF')
+  public openPDF(): void {
+    this.log("openPDF");
     let DATA = this.pdfContent.nativeElement;
     let doc = new jsPDF();
-    
-    this.log(DATA)
-    this.log(DATA.innerHTML)
-  
+
+    this.log(DATA);
+    this.log(DATA.innerHTML);
+
+    doc.setFontSize(12);
+    //doc.text("Octonyan loves jsPDF", 35, 25);*/
+    doc.text(DATA.innerHTML, 0, 12);
+
     doc.html(DATA.innerHTML, {
-      callback: function (doc) {
+      callback: function(doc) {
         doc.save();
-      }
+      },
+      x: 10,
+      y: 10
     });
-    doc.output('dataurlnewwindow');
+
+    /*doc.html(DATA.innerHTML, {
+       callback: function (doc) {
+        doc.save();
+       }
+    });*/
+
+    var elementHandler = {
+      "#ignorePDF": function(element, renderer) {
+        return true;
+      }
+    };
+    /*doc.fromHTML(DATA.innerHTML, 15, 15, {
+      width: 180,
+      elementHandlers: elementHandler
+    });*/
+
+    //doc.fromHTML(DATA.innerHTML, 15, 15);
+
+    doc.output("dataurlnewwindow");
   }
 
-  public downloadPDF():void {
+  public downloadPDF(): void {
     let DATA = this.pdfContent.nativeElement;
     let doc = new jsPDF();
 
     let handleElement = {
-      '#editor':function(element,renderer){
+      "#editor": function(element, renderer) {
         return true;
       }
     };
-    doc.html(DATA.innerHTML,{
-      'width': 200,
-      'elementHandlers': handleElement
+    doc.html(DATA.innerHTML, {
+      width: 200,
+      elementHandlers: handleElement
     });
 
-    doc.save('angular-demo.pdf');
+    doc.save("angular-demo.pdf");
+    doc.output("dataurlnewwindow");
   }
 
-  public captureScreen()  
-  {  
-    var data = document.getElementById('pdfContent');  
-    html2canvas(data).then(canvas => {  
-      // Few necessary setting options  
-      var imgWidth = 208;   
-      var pageHeight = 295;    
-      var imgHeight = canvas.height * imgWidth / canvas.width;  
-      var heightLeft = imgHeight;  
-  
-      const contentDataURL = canvas.toDataURL('image/png')  
-      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
-      var position = 0;  
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
-      pdf.save('MYPdf.pdf'); // Generated PDF   
-    });  
-  }  
-  
+  public captureScreen() {
+    var data = document.getElementById("pdfContent");
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = (canvas.height * imgWidth) / canvas.width;
+      var heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL("image/png");
+      let pdf = new jsPDF("p", "mm", "a4"); // A4 size page of PDF
+      var position = 0;
+      pdf.addImage(contentDataURL, "PNG", 0, position, imgWidth, imgHeight);
+      pdf.save("MYPdf.pdf"); // Generated PDF
+      pdf.output("dataurlnewwindow");
+    });
+  }
+
+  public pdfMake1() {
+    //pdfMake.createPdf(document.getElementById("pdfContent")).open();
+    const documentDefinition = {
+      content: this.pdfContent.nativeElement.innerHTML
+    };
+    pdfMake.createPdf(documentDefinition).open();
+  }
+
+  public generatePdf(action = "open") {
+    const documentDefinition = this.getDocumentDefinition();
+    switch (action) {
+      case "open":
+        pdfMake.createPdf(documentDefinition).open();
+        break;
+      case "print":
+        pdfMake.createPdf(documentDefinition).print();
+        break;
+      case "download":
+        pdfMake.createPdf(documentDefinition).download();
+        break;
+      default:
+        pdfMake.createPdf(documentDefinition).open();
+        break;
+    }
+  }
+
+  public htmlToPdfmake() {
+    this.log(this.pdfContent.nativeElement.innerHTML);
+    var html = htmlToPdfmake(this.pdfContent.nativeElement.innerHTML);
+    this.log(html);
+    pdfMake
+      .createPdf({
+        content: html
+      })
+      .open();
+  }
+
   private log(message: string) {
     this.messagesService.add(`TestPDFComponent: ${message}`);
   }
